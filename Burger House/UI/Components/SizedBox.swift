4833f9c41eb6ee60c35f1d4e3f96634a4b969942
jovi848 <j.mcintyre@senit.com>
2023-03-26 03:57:23 -0400

    created grid view

---
+++ Burger House/UI/Components/SizedBox.swift
@@ -0,0 +1,18 @@
+//
+//  SizedBox.swift
+//  Burger House
+//
+//  Created by Jovi on 26/03/2023.
+//
+
+import SwiftUI
+
+struct SizedBox: View {
+    var height: CGFloat?
+    var width: CGFloat?
+    
+    var body: some View {
+        Color.clear
+            .frame(width: width, height: height)
+    }
+}
--- Burger House/UI/Views/Main/Home.swift
+++ Burger House/UI/Views/Main/Home.swift
@@ -30,9 +30,30 @@ struct HomeView: View {
                 Image("banner")
                     .resizable()
                     .scaledToFit()
+
                 Title("Popular")
                 Subtitle("Top orders in the HOUSE!")
                 
+                VStack{
+                    HStack{
+                        CustomImageView()
+                        SizedBox(width: 8)
+                        CustomImageView()
+                        
+                    }
+                    HStack{
+                        CustomImageView()
+                        SizedBox(width: 8)
+                        CustomImageView()
+                        
+                    }
+                    HStack{
+                        CustomImageView()
+                        SizedBox(width: 8)
+                        CustomImageView()
+                        
+                    }
+                }
                 
 
 
@@ -45,13 +66,40 @@ struct HomeView: View {
     }
 }
 
-struct SizedBox: View {
-    var height: CGFloat?
-    var width: CGFloat?
-    
+
+
+
+struct CustomImageView: View {
     var body: some View {
-        Color.clear
-            .frame(width: width, height: height)
+        ZStack () {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(theme.backgroundColorLight, lineWidth: 2)
+                .background(theme.backgroundColorLight.clipShape(RoundedRectangle(cornerRadius: 20))).padding(.top, 50)
+            
+            Image("burger_2")
+                .resizable()
+                .scaledToFit()
+                .frame(height: 120).padding(.bottom,60)
+           
+
+        }
+        .overlay(
+            VStack{
+                Text("Burger Classic")
+                    .fontWithLineHeight(font: UIFont(name: Constants.poppinsBold, size:16)!, lineHeight: 17)
+                    .foregroundColor(theme.mainFontColor)
+                HStack{
+                    Image("star").resizable().frame(width: 10,height: 10)
+                    Text("$8.00")
+                        .fontWithLineHeight(font: UIFont(name: Constants.poppinsBold, size:16)!, lineHeight: 16)
+                        .foregroundColor(theme.mainSubtleFontColor)
+                    Text("200 cals")
+                        .fontWithLineHeight(font: UIFont(name: Constants.poppinsBold, size:16)!, lineHeight: 16)
+                        .foregroundColor(theme.mainSubtleFontColor)
+                }
+                SizedBox(height: 10)
+
+            }, alignment: .bottom)
+
     }
 }
-
