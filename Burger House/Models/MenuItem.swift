c08b1822d7b892c8912e82d19669de77acbdaa5e
jovi848 <j.mcintyre@senit.com>
2023-03-28 15:25:51 -0400

    added menu item model and pulling data

---
+++ Burger House/Models/MenuItem.swift
@@ -0,0 +1,56 @@
+//
+//  MenuModel.swift
+//  Burger House
+//
+//  Created by Jovi on 28/03/2023.
+//
+
+import SwiftUI
+
+
+class MenuItem {
+    var id: Int
+    var name: String
+    var imageUrl: String
+    var type: String
+    var calories: Int
+    var likes: Int
+    var prepareTime: Int
+    var price: Double
+    var description: String
+    
+    init(id: Int, name: String, imageUrl: String, type: String, calories: Int, likes: Int, prepareTime: Int, price: Double, description: String) {
+        self.id = id
+        self.name = name
+        self.imageUrl = imageUrl
+        self.type = type
+        self.calories = calories
+        self.likes = likes
+        self.prepareTime = prepareTime
+        self.price = price
+        self.description = description
+    }
+    
+    
+    
+    static func getMenuItems()->[MenuItem]{
+         var burgerMenuItems:[MenuItem] = []
+             burgerMenuItems.append(
+                 MenuItem(id: 2, name: "Spicy Jalapeño Burger", imageUrl: "burger_3", type: "Beef Burger", calories: 600, likes: 15, prepareTime: 20, price: 10.99, description: "Our Spicy Jalapeño Burger is the perfect choice for those who love a little heat! Our juicy beef patty is topped with fresh jalapeños, pepper jack cheese, and a spicy mayo that will make your taste buds dance. We use only the highest quality beef for our burgers, and our toppings are always fresh and flavorful. This burger packs a punch with every bite, and is sure to leave you satisfied. Pair it with one of our delicious sides and a cold drink for the ultimate meal. Come try our Spicy Jalapeño Burger today!")
+             )
+
+             burgerMenuItems.append(
+                 MenuItem(id: 3, name: "Mushroom Swiss Burger", imageUrl: "burger_4", type: "Beef Burger", calories: 550, likes: 12, prepareTime: 18, price: 11.99, description: "Our Mushroom Swiss Burger is a classic favorite that never goes out of style. Our juicy beef patty is topped with sautéed mushrooms, melted Swiss cheese, and a delicious garlic aioli that brings all the flavors together. We use only the highest quality beef for our burgers, and our toppings are always fresh and flavorful. This burger is the perfect choice for those who love a rich, savory taste. Pair it with one of our delicious sides and a cold drink for the ultimate meal. Come try our Mushroom Swiss Burger today and see why it's been a customer favorite for years!")
+             )
+
+             burgerMenuItems.append(
+                 MenuItem(id: 4, name: "BBQ Bacon Burger", imageUrl: "burger_5", type: "Beef Burger", calories: 700, likes: 20, prepareTime: 22, price: 12.99, description: "Our BBQ Bacon Burger is a must-try for anyone who loves bold, smoky flavors. Our juicy beef patty is topped with crispy bacon strips, crispy onion rings, cheddar cheese, and tangy BBQ sauce that will have you reaching for more. We use only the highest quality beef for our burgers, and our toppings are always fresh and flavorful. This burger is the perfect choice for those who love a hearty, satisfying meal. Pair it with one of our delicious sides and a cold drink for the ultimate dining experience. Come try our BBQ Bacon Burger today and see why it's one of our most popular items!")
+             )
+
+             burgerMenuItems.append(
+                 MenuItem(id: 5, name: "Buffalo Chicken Burger", imageUrl: "burger_6", type: "Chicken Burger", calories: 550, likes: 18, prepareTime: 20, price: 9.99, description: "Our Buffalo Chicken Burger is a spicy twist on a classic favorite. Our juicy chicken patty is coated in a tangy buffalo sauce, and topped with lettuce, tomato, and blue cheese dressing that will make your taste buds dance. We use only the highest quality chicken for our burgers, and our toppings are always fresh and flavorful. This burger is the perfect choice for those who love a little heat. Pair it with one of our delicious sides and a cold drink for the ultimate meal. Come try our Buffalo Chicken Burger today and see why it's a customer favorite!")
+             )
+             return burgerMenuItems
+     }
+}
+
--- Burger House/UI/Subviews/GridView.swift
+++ Burger House/UI/Subviews/GridView.swift
@@ -23,21 +23,31 @@
 
 import SwiftUI
 
+class Person {
+    var name: String
+    var age: Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+}
 
 struct GridView: View {
     @State var isMenuItem = false
     @State var isDetailViewActive = false
+    @State var index = 0
 
     var body: some View {
         VStack {
             SizedBox(height: 10)
-            ForEach(0..<4) { _ in
+            ForEach(0..<MenuItem.getMenuItems().count/2, id:\.self) { index in
                        HStack {
-                           CustomImageView(isMenuItem: $isMenuItem)
+                           CustomImageView(isMenuItem: $isMenuItem,menuItem:MenuItem.getMenuItems()[index])
                                .navigation(to: DetailView())
                            Spacer()
                                .frame(width: 20)
-                           CustomImageView(isMenuItem: $isMenuItem)
+                           CustomImageView(isMenuItem: $isMenuItem, menuItem: MenuItem.getMenuItems()[index+1])
                                .navigation(to: DetailView())
                        }
                        .padding(.bottom)
@@ -52,6 +62,7 @@ struct GridView: View {
 
 struct CustomImageView: View {
     @Binding var isMenuItem: Bool
+    var menuItem:MenuItem
 
 
     var body: some View {
@@ -70,19 +81,23 @@ struct CustomImageView: View {
             }
             .overlay(
                 VStack{
-                    Text("Burger Classic")
+                    Text(menuItem.name)
                         .fontWithLineHeight(font: UIFont(name: Constants.poppinsBold, size:16)!, lineHeight: 17)
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.1)
                         .foregroundColor(theme.mainFontColor)
+                        .padding(.horizontal,10)
                     if(!isMenuItem){
                         HStack{
                             Image("star").resizable().frame(width: 10,height: 10)
-                            Text("$8.00")
+                            Text(String(format: "$%.2f", menuItem.price))
                                 .fontWithLineHeight(font: UIFont(name: Constants.poppinsBold, size:16)!, lineHeight: 16)
                                 .foregroundColor(theme.mainSubtleFontColor)
-                            Text("200 cals")
+                            Spacer()
+                            Text("\(menuItem.calories) Cals")
                                 .fontWithLineHeight(font: UIFont(name: Constants.poppinsBold, size:16)!, lineHeight: 16)
                                 .foregroundColor(theme.mainSubtleFontColor)
-                        }
+                        }.padding(.horizontal,10)
                     }
                     else{
                         SizedBox(height: 5)
